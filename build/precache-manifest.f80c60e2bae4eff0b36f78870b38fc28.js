a13c4cd9adae7e3b3fc831776abc933b8eb058c4
pcranger <tmhieu2000@gmail.com>
2020-09-15 16:41:28 +0700

    deploy

---
+++ build/precache-manifest.f80c60e2bae4eff0b36f78870b38fc28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9466a94f4796fb3e18716db7d66e9f8",
+    "url": "/learning-reactjs-deploy/index.html"
+  },
+  {
+    "revision": "261346a17186329443b0",
+    "url": "/learning-reactjs-deploy/static/css/main.cca15324.chunk.css"
+  },
+  {
+    "revision": "83780c69b5c5574b9273",
+    "url": "/learning-reactjs-deploy/static/js/2.e752a839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learning-reactjs-deploy/static/js/2.e752a839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261346a17186329443b0",
+    "url": "/learning-reactjs-deploy/static/js/main.a14910ed.chunk.js"
+  },
+  {
+    "revision": "77bd754cd2265f0a3205",
+    "url": "/learning-reactjs-deploy/static/js/runtime-main.0c973e49.js"
+  }
+]);
